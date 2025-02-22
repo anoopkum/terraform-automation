7216ac27ab90737062169aba36f46ce54478d174
anoopkum <loganoop@gmail.com>
2025-02-22 14:07:43 +0000

    removed_untracked_rg

--- main.tf
+++ main.tf
@@ -16,29 +16,6 @@ terraform {
 provider "azurerm" {
   features {}
 }
-resource "azurerm_resource_group" "anoopp_rg02" {
-  name     = "anoopp-rg02"
-  location = "uksouth"
-}
-
-resource "azurerm_resource_group" "anoopp_rg03" {
-  name     = "anoopp-rg03"
-  location = "uksouth"
-}
-resource "azurerm_resource_group" "anoopp_rg04" {
-  name     = "anoopp-rg04"
-  location = "uksouth"
-}
-
-resource "azurerm_resource_group" "anoopp_06" {
-  name     = "anoopp-06"
-  location = "uksouth"
-}
-
-resource "azurerm_resource_group" "anoopp_rg07" {
-  name     = "anoopp-rg07"
-  location = "uksouth"
-}
 
 resource "azurerm_resource_group" "anoopp_rg08" {
   name     = "anoopp-rg08"
