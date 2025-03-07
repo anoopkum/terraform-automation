8efc9403c5f09de0e45422da5d92682a378db89b
anoopkum <loganoop@gmail.com>
2025-03-07 02:04:20 +0000

    Auto-update Terraform configuration

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "rg_test0001-d9371b10" {
-  name     = "rg-test0001-83fbc30f"
-  location = "UK South"
+resource "azurerm_resource_group" "rg_test0001" {
+  name     = "rg-test0001"
+  location = "uksouth"
 }
