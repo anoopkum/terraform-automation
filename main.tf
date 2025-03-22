d9626db183d199f7ac9b5589932a1e3e2b9ada83
anoopkum <loganoop@gmail.com>
2025-03-22 01:57:35 +0000

    Auto-update Terraform configuration

--- main.tf
+++ main.tf
@@ -208,3 +208,16 @@ resource "azurerm_linux_virtual_machine" "vm-standard" {
     ignore_changes = [tags]
   }
 }
+
+resource "azurerm_resource_group" "aiagent_rg" {
+  name     = "aiagent-demo-rg"
+  location = "uksouth"
+
+  tags = {
+    Deploy_via = "TerraformAIAgent"
+  }
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
